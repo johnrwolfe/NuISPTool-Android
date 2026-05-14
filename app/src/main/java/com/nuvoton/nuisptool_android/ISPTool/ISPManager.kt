d1f9f8ca44ec5416a597fd9bbea3320a40efa9c9
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-05-14 15:59:15 -0700

    Prepend report ID on outgoing command
    Windows version sends a 65-byte outgoing command with a report ID of 0.

--- app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
+++ app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
@@ -614,7 +614,7 @@ object ISPManager {
 
  //               cmdArray.set(1, interfaceType.value)//NULINK
 
-                val sendBuffer = cmdArray
+                val sendBuffer = byteArrayOf(0x00) + cmdArray
                 var readBufferStrring = HEXTool.toHexString(sendBuffer)
                 var display = HEXTool.toDisPlayString(readBufferStrring)
 
