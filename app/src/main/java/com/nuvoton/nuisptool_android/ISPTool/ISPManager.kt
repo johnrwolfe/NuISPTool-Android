62cbaa30bdbf7a52adce14200cfc7ff0afb0e904
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-05-15 12:24:34 -0700

    Successive connect commands
    Windows uses successive connect commands to nudge the station into
    update mode.

--- app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
+++ app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
@@ -611,38 +611,33 @@ object ISPManager {
         connection.claimInterface(intf,forceClaim)
         val flushBuffer = ByteArray(64)
         
-        for (i in 1..10) {
-        
-            val flushed = connection.bulkTransfer(
-                readPoint,
-                flushBuffer,
-                flushBuffer.size,
-                100
-            )
-        
-            Log.i("ISPManager", "flush[$i]=$flushed")
-        
-            Thread.sleep(200)
-        }
-            while (isRead != 64) {
+            while (index < 20) {
 
  //               cmdArray.set(1, interfaceType.value)//NULINK
 
                 val sendBuffer = cmdArray
                 var readBufferStrring = HEXTool.toHexString(sendBuffer)
                 var display = HEXTool.toDisPlayString(readBufferStrring)
-
+                packetNumber = (0x00000001).toUInt()
+                val sendBuffer = ISPCommandTool.toCMD(ISPCommands.CMD_CONNECT, packetNumber)
                 val isWrite = connection.bulkTransfer(writePoint, sendBuffer, sendBuffer.size, 0)
                 Log.i("ISPManager", "isWrite=" + isWrite + "    ,sendBuffer:  " + display)
 
                 isRead = connection.bulkTransfer(readPoint, readBuffer,readBuffer.size,100)
+                val allZero = readBuffer.all { it == 0.toByte() }
+                if (!allZero) {
+                    break
+                }else{
+                    Thread.sleep(200)
+                    index++
+                }
                 val dump = readBuffer
                     .take(16)
                     .joinToString(" ") {
                         String.format("%02X", it.toInt() and 0xFF)
                     }
                 
-                ISPManager.lastValidationError = "write=$isWrite read=$isRead\n$dump"
+//                ISPManager.lastValidationError = "write=$isWrite read=$isRead\n$dump"
                 readBufferStrring = HEXTool.toHexString(readBuffer)
                 display = HEXTool.toDisPlayString(readBufferStrring)
                 Log.i("ISPManager", "isRead=" + isRead + "    ,readBuffer:  " + display)
