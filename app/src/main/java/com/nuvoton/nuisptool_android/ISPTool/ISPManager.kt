e8a008d82f4f42d93b68bc17cf1932ca0e4bc086
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-05-17 12:21:34 -0700

    Close USB connection, other cleanup

--- app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
+++ app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
@@ -635,15 +635,20 @@ object ISPManager {
             Log.i("ISPManager", "isWrite=" + isWrite + "    ,sendBuffer:  " + display)
             isRead = connection.bulkTransfer(readPoint, readBuffer,readBuffer.size,100)               
             val isConnect = cmdArray[0] == ISPCommands.CMD_CONNECT.value.toByte()   
-            val expectedPackNo = packetNumber + (0x00000001).toUInt()
-            val resultPackNo =
-                if (isRead == 64)
-                    ISPCommandTool.toPackNo(readBuffer)
+            val expectedPackNo =
+                if (!isConnect)
+                   packetNumber + (0x00000001).toUInt()
                 else
-                    0.toUInt()    
+                   0.toUInt()
+            val resultPackNo =
+                if (!isConnect && isRead == 64)
+                   ISPCommandTool.toPackNo(readBuffer)
+                else
+                   0.toUInt()
             if (!isConnect && (isRead == 64) && (resultPackNo != expectedPackNo)) {
                 Log.i("ISPManager", "Ignoring stale packet $resultPackNo, expected $expectedPackNo")
             } else if (!isConnect && isRead == 64) {
+                connection.close()
                 callback.invoke(readBuffer, false)
                 return
             }
@@ -653,6 +658,7 @@ object ISPManager {
             Log.i("ISPManager", "isRead=" + isRead + "    ,readBuffer:  " + display)             
             if (isConnect && !allZero) {
                 Log.i("ISPManager", "Holfuy entered ISP mode")
+                connection.close()
                 callback.invoke(readBuffer, false)
                 return
             }             
@@ -660,6 +666,7 @@ object ISPManager {
             index++        
             Log.i("ISPManager", "index=" + index)
         }
+        connection.close()
         callback.invoke(readBuffer,false)
     }
 
