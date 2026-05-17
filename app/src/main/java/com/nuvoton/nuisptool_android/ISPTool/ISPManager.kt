ea331ff1d29f754cf7da3f623355bb247b25627c
John R. Wolfe <johnwolfe@mailetechnical.com>
2026-05-17 12:12:47 -0700

    Fix remaining merge conflict issues
    Previous commit failed to remove some merge conflict cruft.  This one
    cleans it out and the resulting build gets through to opening the device
    and failing to locate the config file.

--- app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
+++ app/src/main/java/com/nuvoton/nuisptool_android/ISPTool/ISPManager.kt
@@ -633,7 +633,6 @@ object ISPManager {
             var display = HEXTool.toDisPlayString(readBufferStrring)
             val isWrite = connection.bulkTransfer(writePoint, sendBuffer, sendBuffer.size, 0)
             Log.i("ISPManager", "isWrite=" + isWrite + "    ,sendBuffer:  " + display)
-
             isRead = connection.bulkTransfer(readPoint, readBuffer,readBuffer.size,100)               
             val isConnect = cmdArray[0] == ISPCommands.CMD_CONNECT.value.toByte()   
             val expectedPackNo = packetNumber + (0x00000001).toUInt()
@@ -657,29 +656,6 @@ object ISPManager {
                 callback.invoke(readBuffer, false)
                 return
             }             
-            var readBufferStrring = HEXTool.toHexString(sendBuffer)
-            var display = HEXTool.toDisPlayString(readBufferStrring)
-            val isWrite = connection.bulkTransfer(writePoint, sendBuffer, sendBuffer.size, 0)
-            Log.i("ISPManager", "isWrite=" + isWrite + "    ,sendBuffer:  " + display)
-            isRead = connection.bulkTransfer(readPoint, readBuffer,readBuffer.size,100)               
-            val isConnect = cmdArray[0] == ISPCommands.CMD_CONNECT.value.toByte()   
-            val expectedPackNo = packetNumber + (0x00000001).toUInt()
-            val resultPackNo = ISPCommandTool.toPackNo(readBuffer)    
-            if (!isConnect && (isRead == 64) && (resultPackNo != expectedPackNo)) {
-                Log.i("ISPManager", "Ignoring stale packet $resultPackNo, expected $expectedPackNo")
-            } else if (!isConnect) {
-                callback.invoke(readBuffer, false)
-                return
-            }
-            readBufferStrring = HEXTool.toHexString(readBuffer)
-            display = HEXTool.toDisPlayString(readBufferStrring)
-            Log.i("ISPManager", "isRead=" + isRead + "    ,readBuffer:  " + display)                  
-            val allZero = (isRead == 64) && readBuffer.all { it == 0.toByte() }                
-            if (isConnect && !allZero) {
-                Log.i("ISPManager", "Holfuy entered ISP mode")
-                callback.invoke(readBuffer, false)
-                return
-            }                
             Thread.sleep(200)
             index++        
             Log.i("ISPManager", "index=" + index)
